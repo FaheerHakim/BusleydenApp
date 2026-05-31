912f20d5543d18070131ec277931bfc1f4a7a65d
Faheer Hakim <r0820158@student.thomasmore.be>
2026-05-31 21:51:45 +0200

    feat: setup basic screens and confirmed working android emulator

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+export const WEBFLOW_API_TOKEN = 'd20de867a927836f4d50905eb418d23d8affc1b67d60ed4247528fc7d5eccc63';
+export const PRODUCT_COLLECTION_ID = '6a0cb8dcdf02ab80c4c2b4de';
+export const NEWS_COLLECTION_ID = '6a0f86111b7a24ea0e0ee305';
